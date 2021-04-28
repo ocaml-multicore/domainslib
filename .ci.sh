2265a04e43cc141e7aa409b773f286fae9a4e59d
Anil Madhavapeddy <anil@recoil.org>
2021-04-28 12:01:18 +0100

    Apply suggestions from code review

--- .ci.sh
+++ .ci.sh
@@ -1,8 +1,8 @@
 
 # use opam to install multicore and dune
-opam update
+opam update -u
 opam switch create ${OCAML_VERSION} --packages=ocaml-variants.${OCAML_VERSION} --repositories=multicore=git+https://github.com/ocaml-multicore/multicore-opam.git,default
-eval $(opam config env)
+eval $(opam env)
 opam install dune
 
 # run the tests
