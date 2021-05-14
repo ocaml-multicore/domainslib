a6ed2283906bc2fd75c147db53801726aa8f4947
Tom Kelly <ctk21@cl.cam.ac.uk>
2021-05-14 20:33:39 +0100

    Attempt at wrapping non-blocking polling structure in a blocking

--- lib/multi_channel.ml
+++ lib/multi_channel.ml
@@ -1,10 +1,24 @@
 
 
+type mutex_condvar = {
+  mutex: Mutex.t;
+  condition: Condition.t
+}
+
+let mc_key =
+  Domain.DLS.new_key (fun () ->
+    let m = Mutex.create () in
+    let c = Condition.create () in
+    {mutex=m; condition=c})
+
+type waiting_notified =
+  | Waiting
+  | Notified
+
 type 'a t = {
-  push_index: int Atomic.t;
-  take_index: int Atomic.t;
   mask: int;
   channels: 'a Chan.t array;
+  waiters: (waiting_notified ref * mutex_condvar ) Chan.t;
 }
 
 let rec log2 n =
@@ -14,37 +28,62 @@ let make n =
   let sz = Int.shift_left 1 ((log2 n)+1) in
   assert ((sz > n) && (sz > 0));
   assert (Int.logand sz (sz -1) == 0);
-  { push_index = Atomic.make 0;
-    take_index = Atomic.make 0;
-    mask = sz - 1;
-    channels = Array.init sz (fun _ -> Chan.make_unbounded ()) }
+  { mask = sz - 1;
+    channels = Array.init sz (fun _ -> Chan.make_unbounded ());
+    waiters = Chan.make_unbounded ()
+    }
+
+let check_waiters mchan =
+  match Chan.recv_poll mchan.waiters with
+    | None -> ()
+    | Some (status, mc) ->
+      begin
+        status := Notified;
+        Mutex.lock mc.mutex;
+        Mutex.unlock mc.mutex;
+        Condition.broadcast mc.condition;
+      end
 
 let send mchan v =
-  let i = Atomic.fetch_and_add mchan.push_index 1 in
-  (*Printf.printf "send: %d\n%!" i;*)
-  Chan.send mchan.channels.(Int.logand i mchan.mask) v
+  let id = (Domain.self () :> int) in
+  let res = Chan.send mchan.channels.(Int.logand id mchan.mask) v in
+  check_waiters mchan;
+  res
 
-let recv mchan =
-  let i = Atomic.fetch_and_add mchan.take_index 1 in
-  (*Printf.printf "recv: %d\n%!" i;*)
-  Chan.recv mchan.channels.(Int.logand i mchan.mask)
+let rec recv_poll_loop mchan cur left =
+  if left = 0 then None
+  else begin
+    match Chan.recv_poll mchan.channels.(Int.logand cur mchan.mask) with
+      | Some _ as v -> v
+      | None -> recv_poll_loop mchan (cur+1) (left-1)
+  end
 
-
-(*
- THIS DOESN'T WORK, ANOTHER RECV CAN HOIST THE MESSAGE YOU WANTED
- *)
 let recv_poll mchan =
-  let rec loop () =
-    let push_idx = Atomic.get mchan.push_index in
-    let take_idx = Atomic.get mchan.take_index in
-    (* FIXME: does wrapping behave as expected here? *)
-    if take_idx - push_idx < 0 then
-      if Atomic.compare_and_set mchan.take_index take_idx (take_idx+1) then begin
-        (*Printf.printf "recv_poll: %d\n%!" take_idx;*)
-        Some (Chan.recv mchan.channels.(Int.logand take_idx mchan.mask))
+  recv_poll_loop mchan (Domain.self () :> int) (Array.length mchan.channels)
+
+let rec recv mchan =
+  match recv_poll mchan with
+    | Some v -> v
+    | None ->
+      begin
+        (* Didn't find any thing, prepare to block:
+         *  - enqueue our wait block in the waiter queue
+         *  - check the queue again
+         *  - go to sleep if our wait block has not been notified
+         *  - when notified retry the recieve
+         *)
+        let status = ref Waiting in
+        let mc = Domain.DLS.get mc_key in
+        Chan.send mchan.waiters (status, mc);
+        match recv_poll mchan with
+          | Some v -> v
+          | None ->
+            begin
+               Mutex.lock mc.mutex;
+               while !status = Waiting do
+                 Condition.wait mc.condition mc.mutex
+               done;
+               Mutex.unlock mc.mutex;
+               recv mchan
+            end
       end
-      else loop ()
-    else
-      None
-  in
-  loop ()
