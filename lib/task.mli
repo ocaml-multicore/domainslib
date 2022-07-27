1c78308a1b7788afbcf793b81974e49a493b088b
favonia <favonia@gmail.com>
2022-07-27 07:07:50 -0500

    Update the doc to clarify when the calling domain is used

--- lib/task.mli
+++ lib/task.mli
@@ -26,9 +26,9 @@ val get_num_domains : pool -> int
     including the parent domain. *)
 
 val run : pool -> 'a task -> 'a
-(** [run p t] runs the task [t] synchronously in the pool [p]. If the task [t]
-    blocks on a promise, then tasks from the pool [p] are executed until the
-    promise blocking [t] is resolved.
+(** [run p t] runs the task [t] synchronously with the calling domain and the
+    domains in the pool [p]. If the task [t] blocks on a promise, then tasks
+    from the pool [p] are executed until the promise blocking [t] is resolved.
 
     This function should be used at the top level to enclose the calls to other
     functions that may await on promises. This includes {!await},
@@ -41,23 +41,26 @@ val async : pool -> 'a task -> 'a promise
   *)
 
 val await : pool -> 'a promise -> 'a
-(** [await p r] waits for the promise to be resolved. If the task associated
-    with the promise had completed sucessfully, then the result of the task
-    will be returned. If the task had raised an exception, then [await] raises
-    the same exception.
+(** [await p r] waits for the promise [r] to be resolved. During the resolution,
+    other tasks in the pool [p] might be run using the calling domain and/or the
+    domains in the pool [p]. If the task associated with the promise have
+    completed successfully, then the result of the task will be returned. If the
+    task have raised an exception, then [await] raises the same exception.
 
-    Must be called with a call to {!run} in the dynamic scope. *)
+    Must be called with a call to {!run} in the dynamic scope to handle the
+    internal algebraic effects for task synchronization. *)
 
 val parallel_for : ?chunk_size:int -> start:int -> finish:int ->
                    body:(int -> unit) -> pool -> unit
 (** [parallel_for c s f b p] behaves similar to [for i=s to f do b i done], but
-    runs the for loop in parallel. The chunk size [c] determines the number of
-    body applications done in one task; this will default to [max(1,
-    (finish-start + 1) / (8 * num_domains))]. Individual iterations may be run in
-    any order. Tasks are distributed to workers using a divide-and-conquer
-    scheme.
+    runs the for loop in parallel with the calling domain and/or the domains in
+    the pool [p]. The chunk size [c] determines the number of body applications
+    done in one task; this will default to [max(1, (finish-start + 1) / (8 *
+    num_domains))]. Individual iterations may be run in any order. Tasks are
+    distributed to the participating domains using a divide-and-conquer scheme.
 
-    Must be called with a call to {!run} in the dynamic scope. *)
+    Must be called with a call to {!run} in the dynamic scope to handle the
+    internal algebraic effects for task synchronization. *)
 
 val parallel_for_reduce : ?chunk_size:int -> start:int -> finish:int ->
                 body:(int -> 'a) -> pool -> ('a -> 'a -> 'a) -> 'a -> 'a
@@ -67,14 +70,16 @@ val parallel_for_reduce : ?chunk_size:int -> start:int -> finish:int ->
     the reduce function needs to be commutative and associative in order to
     obtain a deterministic result.
 
-    Must be called with a call to {!run} in the dynamic scope. *)
+    Must be called with a call to {!run} in the dynamic scope to handle the
+    internal algebraic effects for task synchronization. *)
 
 val parallel_scan : pool -> ('a -> 'a -> 'a) -> 'a array -> 'a array
 (** [parallel_scan p op a] computes the scan of the array [a] in parallel with
-    binary operator [op] and returns the result array. Scan is similar to
-    [Array.fold_left] but returns an array of reduced intermediate values. The
-    reduce operations are performed in an arbitrary order and the reduce
-    function needs to be commutative and associative in order to obtain a
-    deterministic result.
+    binary operator [op] and returns the result array, using the calling domain
+    and/or the domains in the pool [p]. Scan is similar to [Array.fold_left]
+    but returns an array of reduced intermediate values. The reduce operations
+    are performed in an arbitrary order and the reduce function needs to be
+    commutative and associative in order to obtain a deterministic result.
 
-    Must be called with a call to {!run} in the dynamic scope. *)
+    Must be called with a call to {!run} in the dynamic scope to handle the
+    internal algebraic effects for task synchronization. *)
