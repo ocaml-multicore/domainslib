9723b500881689292d430e4671d0eaa6e21e2bc9
koonwen <koonwen@gmail.com>
2022-12-23 13:52:36 +0800

    Add improvements and benchmarks to counter

--- test/bench_batched_counter.ml
+++ test/bench_batched_counter.ml
@@ -1,6 +1,6 @@
 module T = Domainslib.Task
 
-let nb = 10_000
+let nb = 1_000_000
 let domains = Domain.recommended_domain_count () - 1
 
 module Bench (C : Counters.S) = struct
@@ -29,20 +29,26 @@ module Bench (C : Counters.S) = struct
 end
 
 module Bench_BC_MPMC = Bench (Counters.BatchedCounter)
+module Bench_BC_Fast = Bench (Counters.BatchedCounterFast)
+module Bench_BC_Faster = Bench (Counters.BatchedCounterFaster)
 module Bench_LockFreeCounter = Bench (Counters.LockfreeCounter)
 module Bench_LockCounter = Bench (Counters.LockCounter)
 
 let () =
   Format.printf "@." ;
-  Format.printf "    num_domains: " ;
+  Format.printf "     num_domains: " ;
   for i = 1 to domains do
     Format.printf " %5i   " i
   done ;
   Format.printf "@." ;
-  Format.printf " BatchedCounter: " ;
-  Bench_BC_MPMC.run () ;
-  Format.printf "LockfreeCounter: " ;
+  Format.printf " LockfreeCounter: " ;
   Bench_LockFreeCounter.run ();
-  Format.printf "    LockCounter: " ;
-  Bench_LockCounter.run ()
+  Format.printf "     LockCounter: " ;
+  Bench_LockCounter.run ();
+  Format.printf "  BatchedCounter: " ;
+  Bench_BC_MPMC.run () ;
+  Format.printf " BatchedCounterF: " ;
+  Bench_BC_Fast.run () ;
+  Format.printf "BatchedCounterFF: " ;
+  Bench_BC_Faster.run ()
 
--- test/counters.ml
+++ test/counters.ml
@@ -1,19 +1,20 @@
 module T = Domainslib.Task
-module Atomic = struct
-  include Atomic
+(* module Atomic = struct *)
+(*   include Atomic *)
 
-  let n = 0.0001
-  let get t = Unix.sleepf n; Atomic.get t
-  let incr t = Unix.sleepf n; Atomic.incr t
-  let decr t = Unix.sleepf n; Atomic.decr t
-  let fetch_and_add t = Unix.sleepf n; Atomic.fetch_and_add t
-  let set t = Unix.sleepf n; Atomic.set t
-end
+(*   let n = 0.0001 *)
+(*   let get t = Unix.sleepf n; Atomic.get t *)
+(*   let incr t = Unix.sleepf n; Atomic.incr t *)
+(*   let decr t = Unix.sleepf n; Atomic.decr t *)
+(*   let fetch_and_add t = Unix.sleepf n; Atomic.fetch_and_add t *)
+(*   let set t = Unix.sleepf n; Atomic.set t *)
+(* end *)
 
 module CounterBase = struct
   module Q = Mpmc_queue
   type t = {
     counter : int Atomic.t;
+    batch_size : int Atomic.t;
     running : bool Atomic.t;
     lock : Mutex.t;
     q : batch_op Q.t;
@@ -30,6 +31,7 @@ module CounterBase = struct
 
   let create n =
     {counter = Atomic.make 0;
+     batch_size = Atomic.make 0;
      running = Atomic.make false;
      q = Q.make ();
      container = Array.make n Null;
@@ -43,6 +45,46 @@ module type S = sig
   val increment : T.pool -> t -> unit
   val decrement : T.pool -> t -> unit
   val get : T.pool -> t -> int
+end
+
+module LockCounter : S = struct
+  include CounterBase
+  let increment _pool t =
+    Mutex.lock t.lock;
+    Atomic.incr t.counter;
+    Mutex.unlock t.lock
+
+  let decrement _pool t =
+    Mutex.lock t.lock;
+    Atomic.decr t.counter;
+    Mutex.unlock t.lock
+
+  let get _pool t =
+    Mutex.lock t.lock;
+    let res = Atomic.get t.counter in
+    Mutex.unlock t.lock;
+    res
+end
+
+module LockfreeCounter : S = struct
+  (* Instead of using the hardware support for fetch-and-add instruction,
+     we simulate how contention degrades as cores increase *)
+  include CounterBase
+  let rec increment _pool t =
+    let v = Atomic.get t.counter in
+    if Atomic.compare_and_set t.counter v (v + 1)
+    then ()
+    else increment _pool t
+
+  let decrement _pool t =
+    let v = Atomic.get t.counter in
+    if Atomic.compare_and_set t.counter v (v - 1)
+    then ()
+    else increment _pool t
+
+  let get _pool t =
+    Atomic.get t.counter
+
 end
 
 module BatchedCounter : S = struct
@@ -99,52 +141,111 @@ module BatchedCounter : S = struct
     T.await pool pr
 end
 
-module LockfreeCounter : S = struct
+module BatchedCounterFast : S = struct
   include CounterBase
-  let rec increment _pool t =
-    let v = Atomic.get t.counter in
-    if Atomic.compare_and_set t.counter v (v + 1)
-    then ()
-    else increment _pool t
+      
+  let rec try_launch pool t =
+  if Atomic.get t.batch_size > 0
+  && Atomic.compare_and_set t.running false true
+  then
+    let len = Atomic.exchange t.batch_size 0 in
+    (match Hashtbl.find_opt stats len with
+     | Some cnt -> Hashtbl.replace stats len (cnt + 1)
+     | None -> Hashtbl.add stats len 1);
+    let cur = Atomic.get t.counter in
+    assert (len > 0);
+    let add_n =
+      T.parallel_for_reduce pool ~start:0 ~finish:(len-1)
+          ~body:(fun _i ->
+              match Q.pop t.q |> Option.get with (* no [batch] or [t.container] array *)
+                | Incr (_, set) -> set (); 1
+                | Decr (_, set) ->  set (); -1
+                | Get (_, set) -> set cur; 0
+                | Null -> failwith "Bad") ( + ) 0 in
+    Atomic.set t.counter (cur + add_n);
+    Atomic.set t.running false;
+    try_launch pool t
 
-  let decrement _pool t =
-    let v = Atomic.get t.counter in
-    if Atomic.compare_and_set t.counter v (v - 1)
-    then ()
-    else increment _pool t
+  let increment pool t =
+    let pr, set = T.promise () in
+    Q.push t.q (Incr (t, set));
+    Atomic.incr t.batch_size;
+    try_launch pool t;
+    T.await pool pr
 
-  let get _pool t =
-    Atomic.get t.counter
+  let decrement pool t =
+    let pr, set = T.promise () in
+    Q.push t.q (Decr (t, set));
+    Atomic.incr t.batch_size;
+    try_launch pool t;
+    T.await pool pr
 
+  let get pool t =
+    let pr, set = T.promise () in
+    Q.push t.q (Get (t, set));
+    Atomic.incr t.batch_size;                      
+    try_launch pool t;
+    T.await pool pr
 end
 
-module LockCounter : S = struct
+module BatchedCounterFaster : S = struct
   include CounterBase
-  let increment _pool t =
-    Mutex.lock t.lock;
-    Atomic.incr t.counter;
-    Mutex.unlock t.lock
+      
+  let rec try_launch pool t =
+  if Atomic.get t.batch_size > 0
+  && Atomic.compare_and_set t.running false true
+  then
+    let len = Atomic.exchange t.batch_size 0 in
+    (* assert (len > 0); *)
+    let add_n =
+      T.parallel_for_reduce pool ~start:0 ~finish:(len-1)
+          ~body:(fun _ ->
+              match Q.pop t.q |> Option.get with (* no [batch] or [t.container] array *)
+                | Incr (_, set) -> set (); 1
+                | Decr (_, set) ->  set (); -1
+                | _ -> failwith "Bad") ( + ) 0 in
+    let _ : int = Atomic.fetch_and_add t.counter add_n in
+    Atomic.set t.running false;
+    try_launch pool t
 
-  let decrement _pool t =
-    Mutex.lock t.lock;
-    Atomic.decr t.counter;
-    Mutex.unlock t.lock
+  let increment pool t =
+    if (* fast path *)
+      let v = Atomic.get t.counter in
+      Atomic.compare_and_set t.counter v (v + 1)
+    then ()
+    else begin (* slow path *)
+      let pr, set = T.promise () in
+      Q.push t.q (Incr (t, set));
+      Atomic.incr t.batch_size;
+      try_launch pool t;
+      T.await pool pr
+    end
 
-  let get _pool t =
-    Mutex.lock t.lock;
-    let res = Atomic.get t.counter in
-    Mutex.unlock t.lock;
-    res
+  let decrement pool t =
+    if (* fast path *)
+      let v = Atomic.get t.counter in
+      Atomic.compare_and_set t.counter v (v - 1)
+    then ()
+    else begin (* slow path *)
+      let pr, set = T.promise () in
+      Q.push t.q (Decr (t, set));
+      Atomic.incr t.batch_size;
+      try_launch pool t;
+      T.await pool pr
+    end
+
+  let get _pool t = Atomic.get t.counter
 end
 
-let () =
-  let open BatchedCounter in
-  let n = 1_000_000 in
-  let t = create n in
-  let pool = T.setup_pool ~num_domains:7 () in
-  T.run pool (fun () ->
-    T.parallel_for pool ~start:1 ~finish:n ~body:
-      (fun _ -> increment pool t)
-    );
-  Hashtbl.iter (fun x y -> Printf.printf "%d -> %d\n" x y) stats;
-  T.teardown_pool pool
+
+(* let () = *)
+(*   let open BatchedCounter in *)
+(*   let n = 1_000_000 in *)
+(*   let t = create n in *)
+(*   let pool = T.setup_pool ~num_domains:8 () in *)
+(*   T.run pool (fun () -> *)
+(*     T.parallel_for pool ~start:1 ~finish:n ~body: *)
+(*       (fun _ -> increment pool t) *)
+(*     ); *)
+(*   Hashtbl.iter (fun x y -> Printf.printf "%d -> %d\n" x y) stats; *)
+(*   T.teardown_pool pool *)
