02c45b71e25e7b92c51ce9086e2520d7c3942c23
Kiran Gopinathan <kirang@comp.nus.edu.sg>
2023-02-12 09:20:46 +0000

    refactored batcher api

--- lib/batcher.mli
+++ lib/batcher.mli
@@ -1,34 +1,44 @@
-module type BatchedDS =
-sig
+module type S = sig
   type t
-  (** Type of the Explicitly Batched Data Structure *)
+  (** [t] represents a vanilla data structure. *)
 
-  type 'a batch_op 
-  (** GADT specifiying the metadata of the operation to be performed including the parameters of the operation exepected *)
+  type 'a op 
+  (** ['a op] represents a single operation on [t] with the return type ['a]. *)
 
-  (* Change constructor to Wrapped *)
-  type wrapped_batch_op =
-      Batched_op : 'a batch_op * ('a -> unit) -> wrapped_batch_op
-    (** Wrapper to make 'a batch_op an existential type so that we can batch different operations together and pass them around easily *)
+  type wrapped_op = Mk : 'a op * ('a -> unit) -> wrapped_op
+  (** [wrapped_op] represents an operation on the datastructure and
+      the continuation to run after its completion.  *)
 
-  val create : unit -> t
-  (** [create pool] returns a new instance of the batched data structure. holding a reference to the pool to provide dynamic multi-threading functionality *)
+  val init : unit -> t
+  (** [init ()] returns a new instance of the data structure. *)
 
-  val bop : t -> Task.pool -> wrapped_batch_op array -> int -> unit
-  (** [bop t pool op_array num] accepts an `op_array` containing `num` arbritrary wrapped_batch_op and performs batch-parallel operations to completion *)
+  val run : t -> Task.pool -> wrapped_op array -> unit
+  (** [run t pool ops num] when called with a data structure [t], and
+      a thread pool [pool], executes all the operations in [ops],
+      possibly using parallelism to improve the speed of the
+      operation. *)
 
 end
-module Make :
-  functor (DS : BatchedDS) ->
-  sig
-    type t
-    type 'a batch_op = 'a DS.batch_op
-    val create : Task.pool -> t
-    (* Change to name to apply *)
-    val batchify : t -> 'a batch_op -> 'a
 
-    val unload : t -> DS.t
-    [@@@alert unsafe "For developer use"]
-    val load : t -> DS.t -> unit
-    [@@@alert unsafe "For developer use"]
-  end
+module Make : functor (S : S) -> sig
+
+  type t
+  (** [t] represents the type of a concurrent data structure *)
+
+  type 'a op = 'a S.op
+  (** ['a op] represents an operation  *)
+
+  val init : Task.pool -> t
+  (** [init pool] creates a new batched data structure, where [pool]
+      will be used for parallelism. *)
+
+  val apply : t -> 'a op -> 'a
+  (** [apply t op] applies the operation [op] to [t]. *)
+
+  val unsafe_get_internal_data : t -> S.t
+  [@@@alert unsafe "For developer use"]
+
+  val unsafe_set_internal_data : t -> S.t -> unit
+  [@@@alert unsafe "For developer use"]
+
+end
