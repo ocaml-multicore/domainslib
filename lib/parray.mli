66fe0451e1dd22c872313aacac27a53631d75f0c
Dario Hett <dariohett+github@gmail.com>
2024-03-02 01:14:13 +0100

    add parameter  to docstrings

--- lib/parray.mli
+++ lib/parray.mli
@@ -3,7 +3,7 @@ type 'a t = 'a array
 (** {1 Iterators} *)
 
 val iter : ('a -> unit) -> 'a array -> Task.pool -> unit
-(** [iter f a] applies function [f] in turn to all
+(** [iter f a p] applies function [f] in turn to all
    the elements of [a].  It is equivalent to
    [f a.(0); f a.(1); ...; f a.(length a - 1); ()]. *)
 
@@ -13,12 +13,12 @@ val iteri :  (int -> 'a -> unit) -> 'a array -> Task.pool -> unit
    and the element itself as second argument. *)
 
 val map :  ('a -> 'b) -> 'a array -> Task.pool -> 'b array
-(** [map f a] applies function [f] to all the elements of [a],
+(** [map f a p] applies function [f] to all the elements of [a],
    and builds an array with the results returned by [f]:
    [[| f a.(0); f a.(1); ...; f a.(length a - 1) |]]. *)
 
 val map_inplace :  ('a -> 'a) -> 'a array -> Task.pool -> unit
-(** [map_inplace f a] applies function [f] to all elements of [a],
+(** [map_inplace f a p] applies function [f] to all elements of [a],
     and updates their values in place.
     @since 5.1 *)
 
